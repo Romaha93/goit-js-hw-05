74997cd486b9bd9d5d954057e4c18da0d27e843f
Romaha93 <raboshroman93@gmail.com>
2024-05-15 17:56:42 +0300

    conspect exercises

--- js/task-1.js
+++ js/task-1.js
@@ -43,3 +43,158 @@ console.log(
   },
 ])
 ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
+
+
+
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(parent.isPrototypeOf(child));
+// console.log(child.hasOwnProperty("age"));
+// console.log(child.hasOwnProperty("name"));
+// console.log(parent.hasOwnProperty("surname"));
+// console.log(child.hasOwnProperty("heritage"));
+// console.log(child.surname);
+// console.log(child.heritage);
+// console.log(child);
+
+
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child);
+
+
+
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// };
+
+// const newCar = new Car("Audi", "Q3", 36000);
+// console.log(newCar)
+
+
+
+// class Car {
+//   #brand;
+//   #model;
+//   #price;
+
+//   constructor(params) {
+//     this.#brand = params.brand;
+//     this.#model = params.model;
+//     this.#price = params.price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set brand(newValue) {
+//     return this.#brand = newValue;
+//   }
+
+//   set model(newValue) {
+//     return this.#model = newValue;
+//   }
+
+//   set price(newValue) {
+//     return this.#price = newValue;
+//   }
+// }
+
+
+
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   static role = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor(params) {
+//     super(params.email);
+//     this.access = params.access;
+//     this.blacklistedEmails = params.blacklistedEmails;
+//   }
+
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+
+//   isBlacklisted(email) {
+//     if (this.blacklistedEmails.includes(email)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+  
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   access: Admin.role.SUPERUSER,
+//   blacklistedEmails: [],
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.access); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
